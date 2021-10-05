6271d7ee04d161dd23e1ab8c4e988df20b9a641d
DESKTOP-JI4G6MQ\User <yr5331@gmail.com>
2021-10-05 23:45:56 +0900

    [fix] html, css, js

--- main.js
+++ main.js
@@ -18,9 +18,49 @@ window.onresize = function(){
 
 // 토글버튼
 const toggleBtn = document.querySelector('.navbar__toogleBtn');
-const menu = document.querySelector('navbar');
 
 toggleBtn.addEventListener('click', () => {
     navbar.classList.toggle('active');
+    const btn = document.getElementById("menu-icon");
+    const burger_class = "fas fa-bars";
+    const close_class = "fas fa-times";
+    btn.className = btn.className ==
+        burger_class ? close_class : burger_class;
 });
+
+/* function navOn() {
+    var nowTop = $(window).scrollTop();
+    var sec1_offset = $('#main').offset().top - 120;
+    var sec2_offset = $('#about-me').offset().top - 120;
+    var sec3_offset = $('#portfolio').offset().top - 120;
+    var sec4_offset = $('#contact').offset().top - 120;
+
+    $('#gnb > li').removeClass('on');
+
+    if (nowTop >= 0 && nowTop < sec2_offset) {
+        $('#main > li').eq(0).addClass('on');
+    } else if (nowTop >= sec2_offset && nowTop < sec3_offset) {
+        $('#about-me > li').eq(2).addClass('on');
+    } else if (nowTop >= sec3_offset && nowTop < sec4_offset) {
+        $('#portfolio > li').eq(3).addClass('on');
+    } else if (nowTop >= sec4_offset) {
+        $('#contact > li').eq(5).addClass('on');
+        
+    }
+};
+navOn();
+
+// 4. scroll function을 넣어 scroll을 트리거로 이벤트를 발생시킨다.
+$(window).scroll(function() {
+    navOn();
+}); */
+
+// Active
+/* var targetLink = document.querySelectorAll('.navbar a');{
+    for( var j=0; j<targetLink.length; j++){
+    targetLink[j].classList.remove('active');
+    document.getElementById('tab-content').classList.add('active');
+    }
+} */
+
 
